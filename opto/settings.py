93eb12c8d99e5aafdd4faca721ecdb055eac7547
Piotr Socha <piotrsocha01@gmail.com>
2020-04-14 22:16:48 +0200

    Fixed hardcoded SECRET_KEY

--- opto/settings.py
+++ opto/settings.py
@@ -12,15 +12,24 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'je)k=v9emidkhlnt39gqk+s773_s@#6p1d8nk0&3vxj2gc#zy#'
+try:
+    from .secret_key import SECRET_KEY
+except ImportError:
+    key = get_random_secret_key()
+    SETTINGS_DIR = os.path.abspath(os.path.dirname(__file__))
+    file_path = os.path.join(SETTINGS_DIR, 'secret_key.py')
+    with open(file_path, 'w') as file:
+        file.write(f"SECRET_KEY = '{key}'")
+    from .secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
